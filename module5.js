4f7eccffd489907688127fcae2b00cc199cf6b26
elliotcross <114322766+elliotcross@users.noreply.github.com>
2022-10-22 21:59:29 -0700

    Update module5.js

--- module5.js
+++ module5.js
@@ -1,16 +1,24 @@
-var a = prompt("Please enter a number from 1 to 10"); // Request user input for variable a
-
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
-var b; // Second variable is going to be the chosen number times i
- {
-  // Do multiplication
-  while (i < 11) {
-	b = a * i  
-    document.write(question + i + "x " + " " + " " + " = ", + b + "<br>");
+var a = prompt("Please enter a number from 0 to 10"); // Request user input for variable a
+var msg = '<h2>Multiplication Table</h2>';            // Initialize variable message so table will have a heading
+var i = 0;                 							// Set counter to 0
+					// Do multiplication
+while (i < 11) {
+    msg += i + ' x ' + a + ' = ' + (i * a) + '<br />';
+    i++;
   }
-}
+
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
 el.innerHTML = msg;
+
+//items from original code not being used...
+/* var table = 3; Changed to user input via prompt */
+/* var operator = 'addition'; Removed addition operator */
+
+/* if (operator === 'addition') {
+
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  } Removed addition as option because this page will be multiplication only */
